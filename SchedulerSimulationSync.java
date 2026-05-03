733a885e5e209015f34018721578ba3add78e301
abdulmajeed779 <445050186@std.psau.edu.sa>
2026-05-03 04:40:11 +0300

    Add ReentrantLock to protect execution log ArrayList

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -41,7 +41,7 @@ class SharedResources {
     // TODO #1: Add a ReentrantLock(s) here to protect critical sections
     // Example: public static final ReentrantLock lock = new ReentrantLock();
     public static final ReentrantLock countersLock = new ReentrantLock();
-
+    public static final ReentrantLock logLock = new ReentrantLock();
     // TODO #2: Add a Semaphore to limit concurrent process execution
     // Example: public static final Semaphore cpuSemaphore = new Semaphore(1);
 
@@ -87,7 +87,13 @@ class SharedResources {
     public static void logExecution(String message) {
         // TODO: Protect this critical section with a lock
         // RACE CONDITION: ArrayList is not thread-safe!
-        executionLog.add(message);
+        // Added lock to protect the ArrayList from ConcurrentModificationException
+        logLock.lock();
+        try {
+            executionLog.add(message);
+        } finally {
+            logLock.unlock();
+        }
     }
 }
 
